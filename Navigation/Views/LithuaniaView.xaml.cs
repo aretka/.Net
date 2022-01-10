121e6be2e88ca90f43a87dd2ba8f4e04794b07b4
aretka <aretask2@gmail.com>
2022-01-10 09:16:06 +0200

    Implement pie chart

--- Navigation/Views/LithuaniaView.xaml.cs
+++ Navigation/Views/LithuaniaView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace Navigation.Views
 {
@@ -23,6 +25,30 @@ namespace Navigation.Views
         public LithuaniaView()
         {
             InitializeComponent();
+            SeriesCollection = new SeriesCollection()
+            {
+                new PieSeries
+                {
+                    Title = "First",
+                    Values = new ChartValues<double> { 124},
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Second",
+                    Values = new ChartValues<double> { 354},
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Third",
+                    Values = new ChartValues<double> { 35},
+                    DataLabels = true
+                }
+            };
+            CovidPieChart.Series = SeriesCollection;
         }
+        public SeriesCollection SeriesCollection { get; set; }
+
     }
 }
